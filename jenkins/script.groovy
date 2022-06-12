fb83cadd6a0e9c49de827ea72670c73ab0bd33cb
Michalkolos <cool.kolos@gmail.com>
2022-06-12 18:43:35 +0200

    More Jenkins tests

---
+++ jenkins/script.groovy
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2022  Michal Kolosowski <michalkoloso@gmail.com>
+ */
+def buildApp() {
+    echo 'Building the application from groovy script...'
+    echo "Using version number ${params.VERSION}"
+}
+
+def testApp() {
+
+}
+
+def deployApp() {
+
+}
+
+
+
+return this
